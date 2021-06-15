a75a241225066354a411934c32118b90629e0da7
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-06-15 15:41:11 -0700

    redone diff-in-means estimation and visualization

--- functions/utils.r
+++ functions/utils.r
@@ -17,6 +17,19 @@ custom_mean_diff <- function(assignment, outcome, control = 1) {
         tidy()
     
     out$outcome <- outcome 
+
+    return(out)
+}
+
+# Only applied to comm_report
+
+custom_mean_report <- function(assignment, df) {
+    
+    out <- difference_in_means(comm_report ~ group, 
+                               condition1 = 1,
+                               condition2 = assignment,
+                               data = df) %>% 
+        tidy()
     
     return(out)
 }
@@ -36,10 +49,60 @@ apply_mean_diff_reply <- function(order){
         bind_rows(
             custom_mean_diff(3, reply_vec[order], 2), # female upvote 
             custom_mean_diff(5, reply_vec[order], 4), # male upvote 
-            custom_mean_diff(2, reply_vec[order], 4), # base comparison 
+            custom_mean_diff(2, reply_vec[order], 4), # base comparison (male = base)
             custom_mean_diff(3, reply_vec[order], 5)
-            ) # upvote comparison 
+            ) # upvote comparison (male = base)
     
     return(out)
     
 }
+
+# Custom ggplot function 
+
+test_group_plot <- function(df, plot_title) {
+    
+    df %>%
+        mutate(term = recode(term,
+                             "group2" = "Female",
+                             "group3" = "Female + Upvotes",
+                             "group4" = "Male",
+                             "group5" = "Male + Upvotes")) %>%
+        ggplot(aes(x = fct_reorder(outcome, estimate), y = estimate, ymax = conf.high, ymin = conf.low)) +
+        geom_pointrange() +
+        labs(title = plot_title, 
+             y = "Estimate",
+             x = "") +
+        facet_wrap(~term, ncol = 4) +
+        coord_flip() +
+        geom_hline(yintercept = 0, linetype = 'dashed', col = 'red')
+}
+
+custom_group_plot <- function(df, plot_title, x_labels) {
+ 
+    df %>%
+        mutate(term = recode(term,
+                     "group2" = "Female",
+                     "group3" = "Female + Upvotes",
+                     "group4" = "Male",
+                     "group5" = "Male + Upvotes")) %>%
+        ggplot(aes(x = fct_reorder(outcome, estimate), y = estimate, ymax = conf.high, ymin = conf.low)) +
+        geom_pointrange() +
+        labs(title = plot_title, 
+         y = "Estimate",
+         x = "",
+         subtitle = "Difference in means between control and treatment groups") +
+        scale_x_discrete(labels = x_labels) +
+        facet_wrap(~term, ncol = 4) +
+        coord_flip() +
+        geom_hline(yintercept = 0, linetype = 'dashed', col = 'red')
+}
+
+# Estimating conditional treatment effects
+
+est_cate <- function(nested_df, assignment) {
+
+    nested_df %>%
+        mutate(model = map(data, ~custom_mean_report(assignment, df = .x))) %>%
+        mutate(group = assignment)
+           
+               }
